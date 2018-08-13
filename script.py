77e903171f2d5c02610aeba8b724d7c520fe6602
git-afsantos <contact.andre.santos@gmail.com>
2018-08-13 20:31:06 +0100

    draft script to convert HAROS JSON into HTML table

--- script.py
+++ script.py
@@ -35,52 +35,45 @@ HTML_QA_TEMPLATE = """
                           <tbody>
                             <tr>
                               <td>Lines of Code</td>
-                              <td data-metric="loc">{}</td>
-                              <td>0</td>
-                              <td>-</td>
+                              <td data-metric="loc">{loc}</td>
+                              <td>0</td><td>-</td>
                             </tr>
                             <tr>
                               <td>Comment/Code Ratio</td>
-                              <td data-metric="comment_ratio"></td>
-                              <td>20%</td>
-                              <td>-</td>
+                              <td data-metric="comment_ratio">{comment_ratio}</td>
+                              <td>20%</td><td>-</td>
                             </tr>
                             <tr>
                               <td>Cyclomatic Complexity (avg.)</td>
-                              <td data-metric="cyclomatic_complexity"></td>
-                              <td>1</td>
-                              <td>15</td>
+                              <td data-metric="cyclomatic_complexity">{cyclomatic_complexity}</td>
+                              <td>1</td><td>15</td>
                             </tr>
                             <tr>
                               <td>Coding Style Violations</td>
-                              <td data-metric="coding_violations"></td>
-                              <td>0</td>
-                              <td>-</td>
+                              <td data-metric="coding_violations">{coding_violations}</td>
+                              <td>0</td><td>-</td>
                             </tr>
                             <tr>
                               <td>Maintainability Index</td>
-                              <td data-metric="maintainability_index"></td>
-                              <td>1</td>
-                              <td>100</td>
+                              <td data-metric="maintainability_index">{maintainability_index}</td>
+                              <td>1</td><td>100</td>
                             </tr>
                             <tr>
                               <td>Class Coupling (avg.)</td>
-                              <td data-metric="class_coupling"></td>
-                              <td>0</td>
-                              <td>5</td>
+                              <td data-metric="class_coupling">{class_coupling}</td>
+                              <td>0</td><td>5</td>
                             </tr>
                             <tr>
                               <td>Depth of Inheritance (avg.)</td>
-                              <td data-metric="depth_inheritance"></td>
-                              <td>0</td>
-                              <td>5</td>
+                              <td data-metric="depth_inheritance">{depth_inheritance}</td>
+                              <td>0</td><td>5</td>
                             </tr>
                           </tbody>
                         </table>
                       </div>"""
 
 
-def from_json_to_html(package_name):
+def from_json_to_html(distro, package_name):
     violations_file = os.path.join(DATA_ROOT, "compliance",
                                    package_name + ".json")
     metrics_file = os.path.join(DATA_ROOT, "metrics", package_name + ".json")
@@ -89,7 +82,14 @@ def from_json_to_html(package_name):
     with open(metrics_file, "r") as json_file:
         metrics_data = json.load(json_file)
     metrics = get_interesting_metrics(metrics_data)
-    metrics["coding_violations"] = len(violations_data)
+    metrics["coding_violations"] = str(len(violations_data))
+    return HTML_QA_TEMPLATE.format(
+        distro=distro,
+        score_stars=gen_html_stars(0),
+        user_stars=gen_html_stars(0),
+        warnings=gen_html_warnings([]),
+        **metrics
+    )
 
 def get_interesting_metrics(data):
     sloc = 0
@@ -113,17 +113,24 @@ def get_interesting_metrics(data):
             coupling.append(value)
         elif metric == "depth_inheritance":
             dit.append(value)
+    sloc = str(sloc)
+    ratios = avg(ratios)
+    ratios = str(ratios) if not ratios is None else ""
     cc = avg(cc)
-    cc = round(cc) if not cc is None else None
+    cc = str(round(cc)) if not cc is None else ""
     mi = avg(mi)
-    mi = round(mi) if not mi is None else None
+    mi = str(round(mi)) if not mi is None else ""
+    coupling = avg(coupling)
+    coupling = str(coupling) if not coupling is None else ""
+    dit = avg(dit)
+    dit = str(dit) if not dit is None else ""
     return {
         "loc": sloc,
-        "comment_ratio": avg(ratios),
+        "comment_ratio": ratios,
         "cyclomatic_complexity": cc,
         "maintainability_index": mi,
-        "class_coupling": avg(coupling),
-        "depth_inheritance": avg(dit)
+        "class_coupling": coupling,
+        "depth_inheritance": dit
     }
 
 def avg(xs):
